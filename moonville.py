01bda6703d588b776c186615fa7b970a3cf623fd
Johan Lindberg <johan@pulp.se>
2013-03-27 13:55:54 +0100

    moonville.py is the main file for starting the game.

---
+++ moonville.py
@@ -0,0 +1,10 @@
+#### Moonville
+
+VERSION = u"Moonville 0.1\u03B1"
+
+class Moonville(object):
+    def __init__(self):
+        print VERSION
+        
+if __name__ == "__main__":
+    Moonville()
