1a0e7c6e9bbeddbf0473d7c1e4d7672657988be7
Johan Lindberg <johan@pulp.se>
2013-04-02 22:43:36 +0200

    Added a build scene.
    There's still some work to be done on the location scene, or I may
    have to re-think it.

--- scenes/build.py
+++ scenes/build.py
@@ -33,44 +33,10 @@ import pyglet
 # Moonville imports
 from constants import *
 import configurable
-import scenes.build
 
-class Location(configurable.Scene):
+class Build(configurable.Scene):
     def __init__(self, moonville):
-        super(Location, self).__init__()
+        super(Build, self).__init__()
         self.moonville = moonville
 
-        # Moon overlay
-        moon = cocos.sprite.Sprite(RESOURCES + "/moon_overlay.png")
-        moon.position = (WIDTH - 100, 100)
-        
-        self.add(cocos.layer.ColorLayer(0, 0, 0, 255), z = 0)
-        self.add(moon, z = 1)
-        self.add(LocationMenu(moonville), z = 2)
-
-
-class LocationMenu(cocos.menu.Menu):
-    def __init__(self, moonville):
-        super(LocationMenu, self).__init__("Choose a location")
-
-        self.moonville = moonville
-        self.game = moonville.game
-
-        self.items = []
-        index = 0
-        for location in self.moonville.locations:
-            self.items.append(cocos.menu.MenuItem(location[:-5],
-                                                  self.location_handler(index)))
-            index += 1
-
-        self.create_menu(self.items,
-                         cocos.actions.ScaleTo(1.2, duration = 0.1),
-                         cocos.actions.ScaleTo(1.0, duration = 0.1))
-
-    def location_handler(self, index):
-        i = index
-        def _callback():
-            self.moonville.location = index
-            build = scenes.build.Build(self.moonville)
-            cocos.director.director.replace(cocos.scenes.transitions.ZoomTransition(build, duration = 2))
-        return _callback
+        print self.moonville.locations[self.moonville.location]
